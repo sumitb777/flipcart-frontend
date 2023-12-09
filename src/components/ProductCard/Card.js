279060f9fc401e6ba3727b3b0284219d6731bbb4
sumitb777 <bidwesumit9@gmail.com>
2023-12-09 13:56:23 +0530

    sticky

--- src/components/ProductCard/Card.js
+++ src/components/ProductCard/Card.js
@@ -215,23 +215,26 @@ const Card = ({ cart, setcart }) => {
                     key={value.id}>
 
                     <div className='col-md-5 col-12  p-md-4 bg-white border d-flex flex-column align-items-center   position-sticky  '>
-                      <img className='productimage text-center   img-fluid mt-md-3 mt-1 '
-                        src={`/images/products/${[value.img]}`} alt='' />
-                      <div className='d-flex col-12 gap-2 mt-md-5 ms-md-4 flex-column flex-md-row   '>
 
-                        <div type='button' className='col-md-6  btn btn-lg
+                      <div className='p-sticky p-md-4  d-flex flex-column align-items-center '>
+                        <img className='productimage text-center   img-fluid mt-md-3 mt-1 '
+                          src={`/images/products/${[value.img]}`} alt='' />
+                        <div className='d-flex col-12 gap-2 mt-md-5 ms-md-4 flex-column flex-md-row   '>
+
+                          <div type='button' className='col-md-6  btn btn-lg
                          btn-warning rounded-0  d-flex justify-content-center align-items-center '
-                          id="liveToastBtn" onClick={() => addtocart(value)}
-                        >
-                          <p className='text-center  mt-2 '>Add To Cart</p>
-                        </div>
+                            id="liveToastBtn" onClick={() => addtocart(value)}
+                          >
+                            <p className='text-center  mt-2 '>Add To Cart</p>
+                          </div>
 
 
-                        <div className='col-md-6 btn btn-lg rounded-0  buybutton d-flex justify-content-center 
+                          <div className='col-md-6 btn btn-lg rounded-0  buybutton d-flex justify-content-center 
                     align-items-center' data-bs-toggle="modal"
-                          data-bs-target="#staticBackdrop"
-                          onClick={() => total(value)}  >
-                          <p className=' text-center mt-2 ' >Buy</p>
+                            data-bs-target="#staticBackdrop"
+                            onClick={() => total(value)}  >
+                            <p className=' text-center mt-2 ' >Buy</p>
+                          </div>
                         </div>
                       </div>
                     </div>
