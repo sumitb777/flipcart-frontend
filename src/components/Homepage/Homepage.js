1bb874be05336e4f42baf2b88239e79dcf1663d8
sumitb777 <bidwesumit9@gmail.com>
2023-12-29 18:12:40 +0530

    HomeCarousel

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Row from './Row'
 import Slide from '../Productslide/Slide'
 import ScrollToTop from '../ScrollToTop'
-
-
+import HomeCarousel from './HomeCarousel'
 
 const Homepage = () => {
 
     return (
         <>
+            <HomeCarousel/>
             <Row />
             <Slide />
             <ScrollToTop />
