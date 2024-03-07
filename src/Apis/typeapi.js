6522d790e0312776577c9923c64080ed3350f374
sumitb777 <bidwesumit9@gmail.com>
2024-03-07 12:17:43 +0530

    api

--- src/Apis/typeapi.js
+++ src/Apis/typeapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react'
-// let url = `https://flipcart-backend.onrender.com/get-product-by-type/`;
-let url = `http://localhost:3030/get-product-by-type/`;
+let url = `https://flipcart-backend.onrender.com/get-product-by-type/`;
+// let url = `http://localhost:3030/get-product-by-type/`;
 
 const typepapi = async (type) =>
     
